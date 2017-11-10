38fac09cde60e22dedc4463e8d5d58cb04fed2b6
Rushi <rushicpatel@gmail.com>
2017-11-10 18:54:43 +0800

    Update to handle paths in IOS11 version of Puthonista 3
    And also to import zip files and move files to a reasonable location

--- read_from_working_copy_app.py
+++ read_from_working_copy_app.py
@@ -3,9 +3,14 @@
 
 # Appex script to copy a git file, folder, or repo from the Working Copy app
 
-import appex, os, shutil
+import appex
+import os
+import shutil
+import zipfile
+import datetime
 
-from_wc = os.path.abspath(os.path.expanduser('from Working Copy'))
+from_wc = os.path.abspath(os.path.expanduser('~/Documents'))
+# print("from_wc: " + from_wc)
 
 
 def main():
@@ -13,20 +18,44 @@ def main():
         file_paths = appex.get_file_paths()
         assert len(file_paths) == 1, 'Invalid file paths: {}'.format(file_paths)
         srce_path = file_paths[0]
+        print(file_paths)
+        pathSplitter = ''
         if '/tmp/' in srce_path:
-            dest_path = srce_path.split('/tmp/')[-1]
+            pathSplitter = '/tmp/'
         else:
-            dest_path = srce_path.split('/Repositories/')[-1]
-        dest_path = os.path.join(from_wc, dest_path)
-        file_path, file_name = os.path.split(dest_path)
-        if not os.path.exists(file_path):
-            os.makedirs(file_path)
-        if os.path.isdir(srce_path):
-            shutil.rmtree(dest_path, ignore_errors=True)
-            print(shutil.copytree(srce_path, dest_path))
+            if '/File Provider Storage/' in srce_path:
+                pathSplitter = '/File Provider Storage/'
+            else:
+                pathSplitter = '/Repositories/'
+        dest_path = srce_path.split(pathSplitter)[-1]
+        if srce_path.endswith('.zip'):
+            foldername = srce_path.split('/')[-1].replace('.zip', '')
+            print('Got me a zip file')
+            zf = zipfile.ZipFile(srce_path)
+            for info in zf.infolist():
+                # print (info.filename)
+                newfile = zf.extract(info)
+                newfilename = newfile.split('/')[-1]
+                file_path = from_wc + '/' + foldername
+                if not os.path.exists(file_path):
+                    os.makedirs(file_path)
+                dest_path = file_path + '/' + newfilename
+                # print('Copy [' + newfile + '] to [' + dest_path + ']')
+                print(shutil.move(newfile, dest_path))
+                shutil
         else:
-            print(shutil.copy2(srce_path, dest_path))
-        print('{} was copied to {}'.format(file_name, file_path))
+            # print(dest_path)
+            dest_path = os.path.join(from_wc, dest_path)
+            # print(dest_path)
+            file_path, file_name = os.path.split(dest_path)
+            if not os.path.exists(file_path):
+                os.makedirs(file_path)
+            if os.path.isdir(srce_path):
+                shutil.rmtree(dest_path, ignore_errors=True)
+                print(shutil.copytree(srce_path, dest_path))
+            else:
+                print(shutil.copy2(srce_path, dest_path))
+            print('{} was copied to {}'.format(file_name, file_path))
     else:
         print('''* In Working Copy app select a repo, file, or directory to be
 copied into Pythonista.  Click the Share icon at the upperight.  Click Run
@@ -36,3 +65,4 @@ directory.'''.replace('\n', ' ').replace('.  ', '.\n* '))
 
 if __name__ == '__main__':
     main()
+    
